328d4d5ea9c8152eb7ae504267a69ccdaa746f5c
zhangxy <1198263001@qq.com>
2023-04-16 01:23:22 +0800

    新增： 日志配置文件：logback-spring.xml mybatis-匹配*** lombok依赖
    使用lombok注解，日志无需定义LoggerFactory

--- oversea-admin/src/main/java/com/lhg/overseaadmin/OverSeasPurchasingApplication.java
+++ oversea-admin/src/main/java/com/lhg/overseaadmin/OverSeasPurchasingApplication.java
@@ -1,14 +1,15 @@
 package com.lhg.overseaadmin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class OverSeasPurchasingApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(OverSeasPurchasingApplication.class);
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
